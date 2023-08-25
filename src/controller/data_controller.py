0eee351bd8244c65518cd778bd158b984a91a3dc
yoodahun <dahun4032@gmail.com>
2023-08-26 00:36:48 +0900

    WICS섹터 크롤링, 수정주가 크롤링 추가

--- src/controller/data_controller.py
+++ src/controller/data_controller.py
@@ -1,17 +1,20 @@
+import time
+
 import pandas as pd
 import numpy as np
 from pandas import DataFrame
-
+from datetime import date
+from dateutil.relativedelta import relativedelta
 from src.crawling.sector_and_indicator_crawler import SectorAndIndicatorCrawler
-from src.db.db_executer import DbExecuter
+from src.db.db_executer import *
+from tqdm import tqdm
 
 class DataController:
     def __init__(self):
         self.sector_and_indicator_crawler = SectorAndIndicatorCrawler()
-        self.db_executor = DbExecuter()
 
 
-    def merge_and_insert_crawled_sector_and_stock_indicator_data(self)->DataFrame:
+    def merge_and_insert_crawled_sector_and_stock_indicator_data(self):
         sector_data = self.sector_and_indicator_crawler.crawling_sector()
         indicator_data = self.sector_and_indicator_crawler.crawling_stock_indicator()
 
@@ -35,7 +38,37 @@ class DataController:
         korean_ticker_data = korean_ticker_data.replace({np.nan:None})
         korean_ticker_data['기준일'] = pd.to_datetime(korean_ticker_data["기준일"])
 
-        self.db_executor.insert_crawled_serctor_and_stock_indicator_data(korean_ticker_data)
+        insert_crawled_serctor_and_stock_indicator_data(korean_ticker_data)
+
+    def insert_wics_sector_info_data(self):
+        sector_data = self.sector_and_indicator_crawler.crawling_wics_sector_info()
+        insert_wics_sector_info_data(sector_data)
+
+    def insert_adjust_stock_price(self):
+        ticker_list = select_ticker_list_where_max_date()
+
+        from_date = (date.today() + relativedelta(days=-1)).strftime("%Y%m%d")
+        to_date = (date.today()).strftime("%Y%m%d")
+        # to_date = from_date
+
+        print(f"{from_date} ~ {to_date}")
+
+        for i in tqdm(range(len(ticker_list))):
+            try:
+                price = self.sector_and_indicator_crawler.crawling_adjust_stock_price(
+                    ticker_list["종목코드"][i], from_date, to_date
+                )
+                insert_adjust_stock_price(price)
+            except Exception:
+                print(f"수정주가 조회 실패 : {ticker_list['종목코드'][i]} {ticker_list['종목명'][i]}")
+
+            time.sleep(0.8)
+
+
+
+
+
+
 
 
 
--- src/crawling/sector_and_indicator_crawler.py
+++ src/crawling/sector_and_indicator_crawler.py
@@ -1,6 +1,9 @@
+import time
+
 import requests as rq
 from io import BytesIO
 import pandas as pd
+from tqdm import tqdm
 from pandas import DataFrame
 
 from src.crawling.biz_day_crawler import biz_day_crawler
@@ -10,12 +13,12 @@ class SectorAndIndicatorCrawler:
     def __init__(self):
         self.gen_otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
         self.header = {
-            "Referer":'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'
+            "Referer": 'http://data.krx.co.kr/contents/MDC/MDI/mdiLoader'
         }
         self.down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
         self.biz_day = biz_day_crawler()
 
-    def crawling_sector(self)-> DataFrame:
+    def crawling_sector(self) -> DataFrame:
         kospi = self.__crawling_sector_kospi()
         kosdaq = self.__crawling_sector_kosdaq()
 
@@ -26,6 +29,11 @@ class SectorAndIndicatorCrawler:
         return krx_sector
 
     def __crawling_sector_kospi(self):
+        """
+        코스피 섹터 크롤링
+
+        :return:
+        """
         gen_otp_stock = {
             'mktId': 'STK',
             'trdDd': self.biz_day,
@@ -38,8 +46,11 @@ class SectorAndIndicatorCrawler:
         otp_kospi = rq.post(self.gen_otp_url, gen_otp_stock, headers=self.header).text
         return self.__download_data(otp_kospi)
 
-
     def __crawling_sector_kosdaq(self):
+        """
+        코스닥 섹터 크롤링
+        :return:
+        """
         gen_otp_stock = {
             'mktId': 'KSQ',
             'trdDd': self.biz_day,
@@ -53,7 +64,13 @@ class SectorAndIndicatorCrawler:
 
         return self.__download_data(otp_kosdaq)
 
-    def crawling_stock_indicator(self)->DataFrame:
+    def crawling_stock_indicator(self) -> DataFrame:
+        """
+        주식 전 종목 기본지표 크롤링
+        종목코드, 종목명, 종가, 등락률, eps, per, 선행 eps, 선행 per, bps, pbr, 주당배당금, 배당수익률
+
+        :return:
+        """
         gen_otp_stock_info_for_request = {
             'searchType': '1',
             'mktId': 'ALL',
@@ -72,7 +89,45 @@ class SectorAndIndicatorCrawler:
 
         return data
 
-    def __download_data(self, code:str,):
+    def __download_data(self, code: str):
         downloaded_sector_info = rq.post(self.down_url, {'code': code}, headers=self.header)
         return pd.read_csv(BytesIO(downloaded_sector_info.content), encoding='EUC-KR')
 
+    def crawling_wics_sector_info(self) -> DataFrame:
+        sector_code = ["G25", "G35", "G50", "G40", "G10", "G20", "G55", "G30", "G15", "G45"]
+
+        data_sector = []
+        for i in tqdm(sector_code):
+            url = f"http://www.wiseindex.com/Index/GetIndexComponets?ceil_yn=0&dt={self.biz_day}&sec_cd={i}"
+            data = rq.get(url).json()
+            data_pd = pd.json_normalize(data["list"])
+
+            data_sector.append(data_pd)
+            time.sleep(1)
+
+        korea_sector = pd.concat(data_sector, axis=0)  # 리스트안에는 데이터프레임 객체들이 들어있는데 그걸 다 합침
+        korea_sector = korea_sector[['IDX_CD', 'CMP_CD', 'CMP_KOR', 'SEC_NM_KOR']]  # 섹터 코드, 티커, 종목명, 섹터명
+        korea_sector['기준일'] = self.biz_day
+        korea_sector['기준일'] = pd.to_datetime(korea_sector['기준일'])
+
+        return korea_sector
+
+    def crawling_adjust_stock_price(self, ticker: str, from_date:str, to_date:str) -> DataFrame:
+
+
+        url = f"https://fchart.stock.naver.com/siseJson.nhn?symbol={ticker}&requestType=1&startTime={from_date}&endTime={to_date}&timeframe=day"
+
+        data = rq.get(url).content
+        data_price = pd.read_csv(BytesIO(data))
+
+        price: DataFrame = data_price.iloc[:, 0:6]
+        price.columns = ["날짜", "시가", "고가", "저가", "종가", "거래량"]
+
+        price = price.dropna()
+
+        price["날짜"] = price["날짜"].str.extract('(\d+)')
+
+        price["날짜"] = pd.to_datetime(price["날짜"])
+        price["종목코드"] = ticker
+
+        return price
