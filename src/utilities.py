f53e300acc3177e8c04d709065bfdaf5932526af
yoodahun <dahun4032@gmail.com>
2023-09-02 15:21:15 +0900

    주피터노트북에서 파일경로를 읽지못하는 문제를 해결하기 위해 절대경로 지정

--- src/utilities.py
+++ src/utilities.py
@@ -3,7 +3,6 @@ import configparser
 
 def get_config(fine_name:str)-> configparser.ConfigParser:
     config = configparser.ConfigParser()
-    ini_file_path=f"./resource/{fine_name}.ini"
+    ini_file_path=f'/Users/yoodahun/Documents/Code/PythonProject/make-quant-portfolio-using-python/resource/{fine_name}.ini'
     config.read(ini_file_path)
-
     return config
