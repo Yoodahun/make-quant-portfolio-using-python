38622029a89f2c22ee1fc68b00e87ecdd6023718
yoodahun <dahun4032@gmail.com>
2024-03-22 19:23:56 +0900

    데이터베이스 정보 관리메소드 추가

--- src/db/db_cursor.py
+++ src/db/db_cursor.py
@@ -4,25 +4,41 @@ from sqlalchemy import create_engine, Engine
 import os
 
 
-def get_db_engine() -> Engine:
+def get_db_info() -> dict:
     is_action = False
-    engine_info = ""
+    db_config = {}
     try:
         is_action = bool(os.environ["IS_GITHUB_ACTION"])
     except KeyError:
         pass
 
     if is_action:
-        engine_info = f'mysql+pymysql://{os.environ["DB_USER"]}:{os.environ["DB_PW"]}@{os.environ["DB_HOST"]}:{os.environ["DB_PORT"]}/{os.environ["DB"]}'
+        db_config["USER"] = os.environ["DB_USER"]
+        db_config["PW"] = os.environ["DB_PW"]
+        db_config["HOST"] = os.environ["DB_HOST"]
+        db_config["PORT"] = os.environ["DB_PORT"]
+        db_config["DB"] = os.environ["DB"]
+
     else:
-        db_config = get_config('db_config')['DB_CONFIG']
-        engine_info = f'mysql+pymysql://{db_config["USER"]}:{db_config["PW"]}@{db_config["HOST"]}:{db_config["PORT"]}/{db_config["DB"]}'
+        db_config["USER"] = get_config('db_config')['DB_CONFIG']["USER"]
+        db_config["PW"] = get_config('db_config')['DB_CONFIG']["PW"]
+        db_config["HOST"] = get_config('db_config')['DB_CONFIG']["HOST"]
+        db_config["PORT"] = get_config('db_config')['DB_CONFIG']["PORT"]
+        db_config["DB"] = get_config('db_config')['DB_CONFIG']["DB"]
+
+    return db_config
+
+
+def get_db_engine() -> Engine:
+    db_config = get_db_info()
+
+    engine_info = f'mysql+pymysql://{db_config["USER"]}:{db_config["PW"]}@{db_config["HOST"]}:{db_config["PORT"]}/{db_config["DB"]}'
 
     return create_engine(engine_info)
 
 
 def db_executemany(query, args):
-    db_config = get_config('db_config')['DB_CONFIG']
+    db_config = get_db_info()
 
     con = pymysql.connect(
         user=db_config["USER"],
