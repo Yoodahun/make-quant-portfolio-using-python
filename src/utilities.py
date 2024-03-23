7c3dd8c0e54c11ee0816559497c86eb325546434
yoodahun <dahun4032@gmail.com>
2024-03-23 14:21:01 +0900

    파일 경로 수정

--- src/utilities.py
+++ src/utilities.py
@@ -9,6 +9,5 @@ def get_config(fine_name: str) -> configparser.ConfigParser:
     path = os.path.join(Path(__file__).parent.parent, "resource")
     ini_file_path = f'{path}/{fine_name}.ini'
 
-    print(ini_file_path)
     config.read(ini_file_path)
     return config
