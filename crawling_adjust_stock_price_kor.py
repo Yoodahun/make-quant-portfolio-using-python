484ac9c428888536d42a63de045d62a0f663bade
yoodahun <dahun4032@gmail.com>
2024-03-23 13:51:37 +0900

    수정주가 크롤링 시 기준일 기준 섹터 및 주식정보도 같이 크롤링실행하도록 저장

--- crawling_adjust_stock_price_kor.py
+++ crawling_adjust_stock_price_kor.py
@@ -3,5 +3,11 @@ from src.controller.data_controller import DataController
 
 controller = DataController()
 
+print("기준일 기준 섹터 및 주식 정보 크롤링 후 저장 ----")
+controller.merge_and_insert_crawled_sector_and_stock_indicator_data()
+#
+print("업종 정보 크롤링 및 합치기 후 저장 ------")
+controller.insert_wics_sector_info_data()
+
 print("수정주가 크롤링 후 저장-----")
 controller.insert_adjust_stock_price()
